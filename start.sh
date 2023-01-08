85014515cdb5911a6d891b32f998eacf54589a2d
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-01-08 17:06:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vijay14958/Channel-Mdisk-Convert.git /Channel-Mdisk-Convert
+  git clone https://github.com/Vijay14958/Mdisk-Try.git /Mdisk-Try
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /Channel-Mdisk-Convert
+  git clone $SOURCE_CODE /Mdisk-Try
 fi
-cd /Channel-Mdisk-Convert
+cd /Mdisk-Try
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
